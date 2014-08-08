63a6b37089177fcd275cb25d46ed9f3f148a3e0f
fazheng <fazheng@b5m.com>
2014-08-08 10:18:54 +0800

    初始话仓库信息

---
+++ 几种线程池的实现算法分析_files/tracker.js
@@ -0,0 +1,51 @@
+var Tracker = {
+	trackVcrImpressions : true,
+	trackForgotPasswordSteps : true,
+	trackContentBookmarking : true,
+
+	doTrackVcrImpressions : function() {
+        var device = CookieManager.readCookie("device");
+
+		if(!Tracker.trackVcrImpressions) {
+			return;
+		}
+		$(".f_vcrbottom").each(
+			function doTrack(index) {
+				var vcr = jQuery.parseJSON($(this).attr("jsh"));
+				_gaq.push(["_trackEvent", (device !== undefined && device == 'mobile') ? "VCRImpressionMobile-"+vcr.id : "VCRImpression-"+vcr.id, $(location).attr("href"), "bottom"]);
+			}
+		);
+		$(".f_vcrembed").each(
+			function doTrack(index) {
+				var vcr = jQuery.parseJSON($(this).attr("jsh"));
+				_gaq.push(["_trackEvent", (device !== undefined && device == 'mobile') ? "VCRImpressionMobile-"+vcr.id : "VCRImpression-"+vcr.id, $(location).attr("href"), "embedded"]);
+			}
+		);
+	},
+	
+	doTrackContentBookmark : function(action, label) {		
+		if(!Tracker.trackContentBookmarking) {
+			return;
+		}		
+		_gaq.push(['_trackEvent', 'Bookmarks', action, label]);
+		
+	},
+	
+	doTrackForgotPasswordSteps : function(step) {
+		if(!Tracker.trackForgotPasswordSteps) {
+			return;
+		}
+		_gaq.push(["_trackEvent", "Forgot password", step, ""]);
+	},
+	
+	safeExec: function(f) {
+		try {
+			f();
+		}
+		catch(ex) {
+			if(window.console && window.console.log) {
+				window.console.log(ex);
+			}
+		}
+	}
+};
